417cd77b6f209e04131ee5d45155af801ea14afe
heejinssss <122508547+heejinssss@users.noreply.github.com>
2023-08-29 10:01:00 +0900

    Update Timer.js

--- frontend/pages/game/mini/liar/Timer.js
+++ frontend/pages/game/mini/liar/Timer.js
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import SockJS from 'sockjs-client';
-import { Stomp } from '@stomp/stompjs';
+import React, { useState, useEffect } from "react";
+import SockJS from "sockjs-client";
+import { Stomp } from "@stomp/stompjs";
 
-/*
-    CONFIRM :: 두더지게임 타이머를 적용할지 고민
-*/
 export default function Timer(){
     const [timeLeft,setTimeLeft]=useState(60);
     const [timerRunning,setTimerRunning]=useState(false);
 
     const socket=new SockJS(process.env.NEXT_PUBLIC_WS + '/ws');
-    //websocket server url 수정 필요
 
     useEffect(()=>{
         const stompClient=Stomp.over(socket);
