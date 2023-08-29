81458e8bd602f84a4343c5fb9dbf98694c97d063
heejinssss <122508547+heejinssss@users.noreply.github.com>
2023-08-29 10:06:51 +0900

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,5 @@
-import { Inter } from 'next/font/google';
-import Link from 'next/link';
 import React, { useEffect } from "react";
-import EnterPage from './enter';
-
-const inter = Inter({ subsets: ['latin'] })
+import EnterPage from "./enter";
 
 export default function Home() {
 
