7e7c56e511714c0174a1a76aa00e8720defb30a3
heejinssss <122508547+heejinssss@users.noreply.github.com>
2023-08-29 11:35:34 +0900

    Update index.js

--- frontend/pages/test4/index.js
+++ frontend/pages/test4/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import React from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 const Dictaphone = () => {
   const {
