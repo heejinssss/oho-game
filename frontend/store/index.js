76e8ad63369f6834e706a31b45361da15bb2b31c
heejinssss <122508547+heejinssss@users.noreply.github.com>
2023-08-29 10:08:01 +0900

    Update index.js

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -3,7 +3,7 @@ import { createWrapper } from "next-redux-wrapper";
 import rootReducer from "./reducers";
 import { persistStore, persistReducer, } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session";
-import logger from 'redux-logger';
+import logger from "redux-logger";
 
 const persistConfig = {
   key: "root",
